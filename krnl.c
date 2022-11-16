b989647ef21c838afcca0878c860ce2a9a11bdfd
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2022-11-16 16:23:54 +0000

    Update krnl.c

--- krnl.c
+++ krnl.c
@@ -1,1 +1,2 @@
 *  welcome! this is EXBOS, we are running c0c4 and v97 of x74, you have downloaded this off github, you have to go to exbos.c1.biz to order a proper version of EXBOS, this computer is NOT USEABLE untill you order a legit version of EXBOS =)
+//load desktop.c/
