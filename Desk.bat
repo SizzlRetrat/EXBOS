b6196f9344a66746e77bd2a9d271c766ff6e035f
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2022-11-16 16:48:42 +0000

    Update Desk.bat

--- Desk.bat
+++ Desk.bat
@@ -1,6 +1,8 @@
 @ echo on
-echo hello we have two apps for you today enter 1 for allapps enter 2 for codeapps
+echo hello we have two apps for you today enter 1 for allapps enter 2 for codeapps enter 3 for yourapp
 :1
 open allapps.bat
 :2
 open apm.bat
+:3
+open
